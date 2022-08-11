0f611d7e6d70124a7c78cff56015196851380adb
Raja0514 <rajamr1987@gmail.com>
2022-08-11 15:10:39 +0400

    added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const dotenv=require('dotenv')
+const path=require('path')
 const connectDB=require('./config/config')
 dotenv.config()
 
@@ -23,11 +24,28 @@ app.use("/api/users",router2)
 const router3=require('./routes/orderRoute')
 app.use("/api/orders",router3)
 
-app.get("/", (req, res) => {
-  res.json("server start up");
-});
+if(process.env.NODE_ENV==='production'){
+
+  app.use(express.static(path.join(__dirname,'/client/build')))
+  
+  app.get('*',(req,res)=>{
+
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+  })
+
+}
+
+else{
+  app.get("/", (req, res) => {
+    res.json("server start up");
+  });
+  
+}
+
 
 const port=process.env.PORT||8080;
+
+
 
 app.listen(port, () => {
   console.log(`Server running on ${process.env.NODE_ENV} mode on port ${process.env.PORT}`);
